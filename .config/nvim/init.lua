ef2a9d0aec2acb5148f3a7c54cfeb00b742a1948
sglre6355 <sglre6355@gmail.com>
2023-04-26 20:10:04 +0100

    Initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,65 @@
+require("plugins")
+
+function gray(component)
+    return {component, color={fg="#a2a3a6"}}
+end
+
+require("lualine").setup({
+    options = {
+        theme = "ayu_dark",
+    },
+    sections = {
+        lualine_c = {gray("filename")},
+        lualine_x = {gray("encoding"), gray("fileformat"), gray("filetype")},
+    }
+})
+
+require("nvim-treesitter.configs").setup({
+    highlight = {
+        enable = true
+    }
+})
+
+vim.g["fern#renderer"] = "nerdfont"
+
+vim.api.nvim_command("colorscheme codedark")
+
+require("neogit").setup()
+require("git-conflict").setup()
+require("gitsigns").setup()
+
+-- Disable Github Copilot for AtCoder codes
+if string.find(vim.api.nvim_buf_get_name(0), "AtCoder")  then
+    vim.api.nvim_command("autocmd VimEnter * Copilot disable")
+end
+
+vim.o.termguicolors = true
+
+vim.o.mouse = "a"
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.hidden = true
+
+vim.o.number = true
+
+vim.o.signcolumn = "yes" 
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.wrapscan = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=40<CR>", {silent = true})
+map("n", "<C-f>", ":Telescope find_files<CR>", {silent = true})
---
+++ README.md
@@ -0,0 +1,23 @@
+# dotfiles
+
+[![LICENSE](https://img.shields.io/github/license/sglre6355/dotfiles)](./LICENSE)
+
+## Required components
+
+- [zsh](https://www.zsh.org/)
+- [Git](https://git-scm.com/)
+- [Neovim](https://neovim.io/)
+- [Node.js](https://nodejs.org/) -- required by coc.nvim
+- [npm](https://www.npmjs.com/) -- required by coc.nvim
+
+## Install
+
+```sh
+$ cd
+$ git clone https://github.com/sglre6355/dotfiles
+$ ./dotfiles/bin/install-dotfiles
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
