6d8f3053fed87531c658870d50a3a795cdabec1f
sglre6355 <sglre6355@gmail.com>
2024-01-03 01:38:22 +0000

    Update neovim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,23 +3,26 @@ require("plugins")
 require("lazy").setup("plugins")
 
 function gray(component)
-    return {component, color={fg="#a2a3a6"}}
+	return { component, color = { fg = "#a2a3a6" } }
 end
 
 require("lualine").setup({
-    options = {
-        theme = "ayu_dark",
-    },
-    sections = {
-        lualine_c = {gray("filename")},
-        lualine_x = {gray("encoding"), gray("fileformat"), gray("filetype")},
-    }
+	options = {
+		theme = "ayu_dark",
+	},
+	sections = {
+		lualine_c = { gray("filename") },
+		lualine_x = { gray("encoding"), gray("fileformat"), gray("filetype") },
+	},
 })
 
 require("nvim-treesitter.configs").setup({
-    highlight = {
-        enable = true
-    }
+	autotag = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+	},
 })
 
 vim.api.nvim_command("colorscheme codedark")
@@ -28,9 +31,117 @@ require("neogit").setup()
 require("git-conflict").setup()
 require("gitsigns").setup()
 
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol",
+			maxwidth = 50,
+			ellipsis_char = "...",
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
+})
+
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "git" },
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+require("mason-lspconfig").setup()
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
+	end,
+})
+
+vim.diagnostic.config({ update_in_insert = true })
+
+require("lint").linters_by_ft = {
+	python = { "flake8" },
+}
+
+local util = require("formatter.util")
+
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+		html = {
+			function()
+				return {
+					exe = "~/.local/share/nvim/mason/bin/prettier",
+					args = {
+						"--tab-width 4",
+						"--html-whitespace-sensitivity ignore",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+					},
+					stdin = true,
+				}
+			end,
+		},
+		python = {
+			require("formatter.filetypes.python").black,
+			require("formatter.filetypes.python").isort,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+	},
+})
+
+require("copilot").setup({
+	suggestion = {
+		enabled = false,
+	},
+})
+
 -- Disable Github Copilot for AtCoder codes
-if string.find(vim.api.nvim_buf_get_name(0), "AtCoder")  then
-    vim.api.nvim_command("autocmd VimEnter * Copilot disable")
+if string.find(vim.api.nvim_buf_get_name(0), "AtCoder") then
+	vim.api.nvim_command("autocmd VimEnter * Copilot disable")
 end
 
 vim.o.termguicolors = true
@@ -43,7 +154,7 @@ vim.o.hidden = true
 
 vim.o.number = true
 
-vim.o.signcolumn = "yes" 
+vim.o.signcolumn = "yes"
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -54,11 +165,11 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", "<C-f>", ":Telescope find_files<CR>", {silent = true})
+map("n", "<C-f>", ":Telescope find_files<CR>", { silent = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,24 +1,50 @@
 return {
-    -- Neovim
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-    "BurntSushi/ripgrep",
-    "nvim-tree/nvim-web-devicons",
-    "nvim-treesitter/nvim-treesitter",
-    "neoclide/coc.nvim",
-    "sharkdp/fd",
-    "nvim-lualine/lualine.nvim",
-    "tomasiser/vim-code-dark",
+	-- Neovim
+	"nvim-telescope/telescope.nvim",
+	"nvim-lua/plenary.nvim",
+	"BurntSushi/ripgrep",
+	"nvim-tree/nvim-web-devicons",
+	"nvim-treesitter/nvim-treesitter",
+	"sharkdp/fd",
+	"nvim-lualine/lualine.nvim",
+	"tomasiser/vim-code-dark",
 
-    -- Coding assistance
-    "github/copilot.vim",
+	-- Autocompletion
+	"hrsh7th/nvim-cmp",
 
-    -- Git
-    { "NeogitOrg/neogit", requires = "nvim-lua/plenary.nvim" },
-    { "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" },
-    { "akinsho/git-conflict.nvim", version = "*", config = true },
-    "lewis6991/gitsigns.nvim",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
 
-    -- Python
-    "brentyi/isort.vim"
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+
+	"onsails/lspkind.nvim",
+
+	"windwp/nvim-ts-autotag",
+
+	-- LSP
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"tamago324/nlsp-settings.nvim",
+
+	-- Linter
+	"mfussenegger/nvim-lint",
+
+	-- Formatter
+	"mhartington/formatter.nvim",
+
+	-- Coding assistance
+	"zbirenbaum/copilot.lua",
+
+	-- Git
+	{ "NeogitOrg/neogit", requires = "nvim-lua/plenary.nvim" },
+	{ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" },
+	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+	"lewis6991/gitsigns.nvim",
+
+    -- LaTeX
+    "lervag/vimtex",
 }
