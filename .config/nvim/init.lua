06f88a707ae66b76b429be0bbcfacafad3827dbf
sglre6355 <sglre6355@gmail.com>
2024-01-11 20:18:47 +0000

    Add lazy.nvim bootstrap code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,21 @@
 require("plugins")
 
+-- Bootstrap for lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup("plugins")
 
 function gray(component)
