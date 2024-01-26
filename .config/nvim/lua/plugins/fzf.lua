3d96053d8e75af5cca8d26047b32f58da7c7c4d5
sglre6355 <sglre6355@gmail.com>
2024-01-26 05:08:53 +0000

    Update telescope config to ignore node_modules

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,3 +1,7 @@
 return {
-    "nvim-telescope/telescope.nvim",
+    { "nvim-telescope/telescope.nvim", opts = {
+        defaults = {
+            file_ignore_patterns = { "node%_modules/.*" },
+        },
+    } },
 }
