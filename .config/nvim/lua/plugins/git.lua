b20abbe1c8182a96089b6f0735c1c27b0ba2bed3
sglre6355 <sglre6355@gmail.com>
2024-07-16 15:48:11 +0900

    Remove neogit from neovim config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,4 @@
 return {
-    { "NeogitOrg/neogit", dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
     { "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim", opts = {} },
     { "akinsho/git-conflict.nvim", opts = {}, version = "*" },
     { "lewis6991/gitsigns.nvim", opts = {} },
