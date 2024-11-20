dc3764e5e7fba404f7a54d60aad773782e9b80e3
sglre6355 <sglre6355@gmail.com>
2024-11-20 02:57:26 +0000

    Replace eval with source for command completions

--- .config/zsh/completions.zsh
+++ .config/zsh/completions.zsh
@@ -1,19 +1,19 @@
 # GitHub CLI
 if type gh > /dev/null; then
-    eval "$(gh completion -s zsh)"
+    source <(gh completion -s zsh)
 fi
 
 # Arduino CLI
 if type arduino-cli > /dev/null; then
-    eval "$(arduino-cli completion zsh)"
+    source <(arduino-cli completion zsh)
 fi
 
 # kubectl
 if type kubectl > /dev/null; then
-    eval "$(kubectl completion zsh | head -n -4)"
+    source <(kubectl completion zsh | head -n -4)
 fi
 
 # Tailscale
 if type tailscale > /dev/null; then
-    eval "$(tailscale completion zsh | head -n -4)"
+    source <(tailscale completion zsh | head -n -4)
 fi
